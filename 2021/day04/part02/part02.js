6947ea3fe7eb536e11c08a4e64fc91d7bc037df5
Philthy-Phil <phil.ziegelbauer@gmx.at>
2021-12-11 14:52:48 +0100

    added day04 part2

--- 2021/day04/part02/part02.js
+++ 2021/day04/part02/part02.js
@@ -4,15 +4,15 @@ const InputReader = require('../InputReader');
 const ir = new InputReader(__dirname, '_input.txt');
 
 const input = ir.getRawData()
-                    .split('\n\n')
-                    .filter(line => Boolean(line))
-                    .map(line => line
-                        .trim()
-                        .replace(/[\n ,]+/g, ' ')
-                        .split(' ')
-                        .map(l => parseInt(l))
-                    );
-                    
+    .split('\n\n')
+    .filter(line => Boolean(line))
+    .map(line => line
+        .trim()
+        .replace(/[\n ,]+/g, ' ')
+        .split(' ')
+        .map(l => parseInt(l))
+    );
+
 let [drawnNumbers, ...cards] = input;
 
 class Card {
@@ -30,6 +30,7 @@ class Card {
         this.lines = Array(this.cardSize).fill(0);
         this.columns = Array(this.cardSize).fill(0);
         this.isComplete = false;
+        this.markedNumbers = new Set();
     }
 
     addMarkedNumber(number) {
@@ -37,6 +38,7 @@ class Card {
         if (!pos) {
             return;
         }
+        this.markedNumbers.add(number);
         this.lines[pos.line]++;
         this.columns[pos.column]++;
         if (this.lines[pos.line] === this.cardSize || this.columns[pos.column] === this.cardSize) {
@@ -44,38 +46,38 @@ class Card {
         }
     }
 
-    showMap() {
-        for (const i of this.cardMap) {
-            console.log(i, this.cardMap.get(i));
-        }
+    unmarkedNumbers() {
+        return this.numbers.filter(n => !this.markedNumbers.has(n));
     }
 }
 
-function part1(_cards) {
-    cards = cards.map(nums => new Card(nums));
-    
-    let winningCard;
+function part2(_cards) {
+    let cards = _cards.map(nums => new Card(nums));
+    let lastWinningCard;
+    let lastWinningNumber;
     const actuallyDrawn = [];
     for (const drawn of drawnNumbers) {
-        let finished = false;
         actuallyDrawn.push(drawn);
-        for (const card of cards) {   
-            card.addMarkedNumber(drawn);
-            if (card.isComplete) {
-                finished = true;
-                winningCard = card;
-                break;
+        let hasIncompleteCards = false;
+        for (const card of cards) {
+            if (!card.isComplete) {
+                hasIncompleteCards = true;
+                card.addMarkedNumber(drawn);
+                if (card.isComplete) {
+                    lastWinningCard = card;
+                    lastWinningNumber = drawn;
+                }
             }
         }
-        if (finished) {
+        if (!hasIncompleteCards) {
             break;
         }
     }
-    
-    const unmarkedNumbers = winningCard.numbers.filter(n => !actuallyDrawn.includes(n));
-    const finalScore = unmarkedNumbers.reduce((a, b) => a + b, 0) * actuallyDrawn.slice(-1);
-            
-    console.log('part01 -> ', finalScore);
+
+    const unmarkedNumbers = lastWinningCard.unmarkedNumbers();
+    const finalScore = unmarkedNumbers.reduce((a, b) => a + b, 0) * lastWinningNumber;
+
+    console.log('part02 -> ', finalScore);
 }
 
-part1(cards);
+part2(cards);
