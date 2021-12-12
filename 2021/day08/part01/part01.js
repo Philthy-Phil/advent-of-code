3624b8bf58eba2fb0678ca76d0c644af142179fc
Philthy-Phil <phil.ziegelbauer@gmx.at>
2021-12-12 08:50:42 +0100

    setup day08

--- 2021/day08/part01/part01.js
+++ 2021/day08/part01/part01.js
@@ -10,20 +10,9 @@ let positions = ir
   .map((n) => Number(n));
 
 function part1() {
-  let fuel = 0;
-  let fuelSums = [];
-
-  for (let i = 0; i < positions.length; i++) {
-    let toPos = i;
-    let fuelSum = 0;
-    positions.forEach((pos) => {
-      fuelSum += Math.abs(pos - toPos);
-    });
-    fuelSums.push(fuelSum);
-  }
-
-  let cheapestFuel = Math.min(...fuelSums);
-  console.log("part01 -> ", cheapestFuel);
+  
+  
+  // console.log("part01 -> ", 0);
 }
 
 part1();
