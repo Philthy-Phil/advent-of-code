103035cc7972edd36f4a13866e740c8605db0699
Philthy-Phil <phil.ziegelbauer@gmx.at>
2021-12-15 21:06:18 +0100

    cleaned day14 part02

--- 2021/day14/part02/part02.js
+++ 2021/day14/part02/part02.js
@@ -13,55 +13,11 @@ let _input = ir
     return { template, pairs };
   });
 
-function findFrequent(arr) {
-
-  let uniques = new Set(arr);
-  let charsMap = new Map();
-  
-  for (const char of uniques) {
-    charsMap.set(char, 0);
-  }
-
-  for (let i = 0; i < arr.length; i++) {
-    for (let [key, value] of charsMap) { 
-        if (arr[i] === key) {
-          value++;
-          charsMap.set(key, value);
-        }
-    }
-  }
-
-  const charsMapSort = new Map([...charsMap.entries()].sort((a, b) => a[1] - b[1]));
-  const leastFrequent = [...charsMapSort][0][1]
-  const mostFrequent = [...charsMapSort][[...charsMapSort].length - 1][1]
-
-  return [leastFrequent, mostFrequent];
-}
-
+´
 function part2(input) {
 
-  let startTemplate = input[0].template.split('');
-  let newArr = [startTemplate[0]];
-  let count = 0;
-
-  while(count < 40) {
-    for (let i = 0; i < startTemplate.length; i++) {
-      for (const pairs of input[0].pairs) {
-        let [combiChars, insertChar] = pairs.split(' -> ')
-        if ((startTemplate[i] + startTemplate[i+1]) === combiChars) {
-          newArr.push(insertChar, startTemplate[i + 1])
-        }
-      }
-    }
-    startTemplate = [...newArr];
-    newArr = [startTemplate[0]];
-    count++;
-  }
-
-  const [leastFrequent, mostFrequent] = findFrequent(startTemplate);
-  const result = mostFrequent - leastFrequent;
-
-  console.log("part02 -> ", result);
+ 
+  console.log("part02 -> ", 0);
 }
 
 part2(_input);
